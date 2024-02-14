601a0a33eb74a603b0377b4ecb10f3ac6c23c189
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-14 13:56:26 +0100

    Update Terraform files

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "instance" {
   availability_zone           = "us-west-2a"
   associate_public_ip_address = true
   key_name                    = "vockey"
-  vpc_security_group_ids      = ["sg-06c6b023048ee9ad9"]
-  subnet_id                   = "subnet-07b832ebd68ea0cda"
+  vpc_security_group_ids      = ["sg-0dbed35222cb4e2f7"]
+  subnet_id                   = "subnet-079808b6cfcaad7a4"
   # iam_instance_profile      = "LabRole"
   count = 1
   tags = {
