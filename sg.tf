954f181d724900963a84cea68e59c002b5bdca15
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 13:03:19 +0100

    add sg rules allowing ssh & http

---
+++ sg.tf
@@ -0,0 +1,25 @@
+# Create a Security Group for the VPC
+resource "aws_security_group" "sg_vpc" {
+  name        = "sg_vpc"
+  description = "allow shh"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  # Add inbound rules
+  # Add a rule for HTTP
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Add a rule for SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
