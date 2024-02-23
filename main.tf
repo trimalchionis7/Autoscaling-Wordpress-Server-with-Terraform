6ce89dcc1acc52d37a27ef2effda13a8fa26c1a9
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 15:36:35 +0100

    removing invalid characters

--- main.tf
+++ main.tf
@@ -69,24 +69,24 @@ tags = {
 # Associate public & private route tables with subnets
 resource "aws_route_table_association" "Public_Subnet1_Asso" {
   route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-${count.index + 1}.id
+  subnet_id      = aws_subnet.public-1.id
   depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
 }
 
 resource "aws_route_table_association" "Private_Subnet1_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-${count.index + 1}.id
+  subnet_id      = aws_subnet.private-1.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
 }
 
 resource "aws_route_table_association" "Public_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-${count.index + 1}.id
+  subnet_id      = aws_subnet.public-2.id
   depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
 }
 
 resource "aws_route_table_association" "Private_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-${count.index + 1}.id
+  subnet_id      = aws_subnet.private-2.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
 }
