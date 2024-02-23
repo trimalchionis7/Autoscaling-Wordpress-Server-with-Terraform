ec25f42730b2d7ab7b832723255ad82cd645a1fb
Jonnie <132094293+trimalchionis7@users.noreply.github.com>
2024-02-23 11:13:07 +0100

    Update az in ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "instance" {
   ami = "ami-0d442a425e2e0a743"
   instance_type               = "t3.micro"
-  availability_zone           = "us-east-2a"
+  availability_zone           = "us-west-2a"
   associate_public_ip_address = true
   key_name                    = "vockey"
   vpc_security_group_ids      = ["sg-0dbed35222cb4e2f7"]
