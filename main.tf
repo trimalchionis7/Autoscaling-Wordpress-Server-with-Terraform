19da9a9894f6a432de4cec47eb02592b3607eb37
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 12:47:13 +0100

    Change CIDR block to allow all traffic

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "aws_route_table" "RB_Public_RouteTable" {
   vpc_id = aws_vpc.dev_vpc.id
 
   route {
-    cidr_block = var.CIDR_BLOCK
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
