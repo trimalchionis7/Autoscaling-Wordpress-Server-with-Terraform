53828e4644b60d8cd816cb9209e456922b390550
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 13:19:02 +0100

    Correct syntax for ingress resource

--- sg.tf
+++ sg.tf
@@ -24,3 +24,15 @@ resource "aws_security_group" "sg_vpc" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  ingress {
+    description      = "allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
