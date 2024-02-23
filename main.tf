0a2f78deb801a872d0f3e8ea40bffad30ddb22f7
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 15:32:23 +0100

    reinserting private rt, correcting declarations to rt

--- main.tf
+++ main.tf
@@ -54,39 +54,39 @@ resource "aws_route_table" "RB_Public_RouteTable" {
   }
 }
 
-# resource "aws_route_table" "RB_Private_RouteTable" {
-# vpc_id = aws_vpc.dev_vpc.id
+resource "aws_route_table" "RB_Private_RouteTable" {
+vpc_id = aws_vpc.dev_vpc.id
 #
 # route { 
 #   cidr_block = "0.0.0.0/0"
 #   gateway_id = aws_internet_gateway.igw.id
 #  }
-# tags = {
-#    Name = "private-rt"
-#  }
-# }
+tags = {
+  Name = "private-rt"
+  }
+}
 
 # Associate public & private route tables with subnets
 resource "aws_route_table_association" "Public_Subnet1_Asso" {
   route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-1.id
+  subnet_id      = aws_subnet.public-${count.index + 1}.id
   depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
 }
 
 resource "aws_route_table_association" "Private_Subnet1_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-1.id
+  subnet_id      = aws_subnet.private-${count.index + 1}.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
 }
 
 resource "aws_route_table_association" "Public_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Public_RouteTable.id
-  subnet_id      = aws_subnet.public-2.id
+  subnet_id      = aws_subnet.public-${count.index + 1}.id
   depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
 }
 
 resource "aws_route_table_association" "Private_Subnet2_Asso" {
   route_table_id = aws_route_table.RB_Private_RouteTable.id
-  subnet_id      = aws_subnet.private-2.id
+  subnet_id      = aws_subnet.private-${count.index + 1}.id
   depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,22 @@
 # variables.tf
 
+# variables.tf
+
 variable "vpc_cidr_block" {
-    description = "CIDR block for the VPC"
-    default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr_blocks" {
-    description = "CIDR blocks for the public subnets"
-    default = ["10.0.1.0/24", "10.0.3.0/24"]
+  description = "CIDR blocks for public subnets"
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidr_blocks" {
-    description = "CIDR blocks for the private subnets"
-    default = ["10.0.2.0/24", "10.0.4.0/24"]
+  description = "CIDR blocks for private subnets"
+  default     = ["10.0.2.0/24", "10.0.4.0/24"]
 }
+
 variable "AWS_REGION" {
     default = "us-west-2"
     description = "AWS Region"
