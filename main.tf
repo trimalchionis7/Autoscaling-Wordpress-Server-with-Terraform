a273e81529b7cf527ceecd6d06b33f2c3b800d23
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 12:38:17 +0100

    Adding route tables

--- main.tf
+++ main.tf
@@ -51,3 +51,61 @@ resource "aws_subnet" "private-2" {
     Name = "deham10" 
   }
 }
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "deham10"
+  }
+}
+
+# Create public & private route tables
+resource "aws_route_table" "RB_Public_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = var.CIDR_BLOCK
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "deham10"
+  }
+}
+
+resource "aws_route_table" "RB_Private_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route { 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "deham10"
+  }
+}
+
+# Associate public & private route tables with subnets
+resource "aws_route_table_association" "Public_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-1.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
+}
+
+resource "aws_route_table_association" "Private_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-1.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
+}
+
+resource "aws_route_table_association" "Public_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-2.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
+}
+
+resource "aws_route_table_association" "Private_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-2.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
+}
