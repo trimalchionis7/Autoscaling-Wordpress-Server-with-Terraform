801ee6281ba1e09e37a8b103cadd066a1898e132
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 10:58:00 +0100

    updating region & name

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "instance" {
   ami = "ami-0d442a425e2e0a743"
   instance_type               = "t3.micro"
-  availability_zone           = "us-west-2a"
+  availability_zone           = "us-east-2a"
   associate_public_ip_address = true
   key_name                    = "vockey"
   vpc_security_group_ids      = ["sg-0dbed35222cb4e2f7"]
@@ -11,6 +11,6 @@ resource "aws_instance" "instance" {
   # iam_instance_profile      = "LabRole"
   count = 1
   tags = {
-    Name = "first-try"
+    Name = "first-vpc"
   }
 }
