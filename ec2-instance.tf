a417dbdc462926d6131f516b0f1a4e2ec279ba21
trimalchionis7 <je.griffiths@live.co.uk>
2024-02-23 11:53:33 +0100

    add subnets to main.tf file

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,11 +3,11 @@
 resource "aws_instance" "instance" {
   ami = "ami-0d442a425e2e0a743"
   instance_type               = "t3.micro"
-  availability_zone           = "us-east-2a"
+  availability_zone           = "us-west-2a"
   associate_public_ip_address = true
   key_name                    = "vockey"
-  vpc_security_group_ids      = ["sg-0dbed35222cb4e2f7"]
-  subnet_id                   = "subnet-079808b6cfcaad7a4"
+  vpc_security_group_ids      = [aws_security_group.sg_vpc.id]
+  subnet_id                   = aws_subnet.public-1.id
   # iam_instance_profile      = "LabRole"
   count = 1
   tags = {
--- main.tf
+++ main.tf
@@ -8,3 +8,46 @@ resource "aws_vpc" "dev_vpc" {
     name     = "deham10"
   }       
 }
+
+# Create 2 public & 2 private subnets in 2 different AZ
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham10"
+  }
+}
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "deham10" 
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham10"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "deham10" 
+  }
+}
